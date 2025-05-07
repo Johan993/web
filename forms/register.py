ac64af9ceea8ebb6041c62a91c6297ed343f00c0
redwec34 <redwec.tr@gmail.com>
2025-05-07 18:03:49 +0300

    Изменение окна регистрации

--- forms/register.py
+++ forms/register.py
@@ -4,13 +4,11 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    surname = StringField('Фамилия', validators=[DataRequired()])
-    name = StringField('Имя', validators=[DataRequired()])
-    age = IntegerField('Возраст', validators=[DataRequired()])
-    position = StringField('Должность', validators=[DataRequired()])
-    speciality = StringField('Специальность', validators=[DataRequired()])
-    address = StringField('Место на корабле (module_1, ...)', validators=[DataRequired()])
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_repeat = PasswordField('Повторите пароль', validators=[DataRequired()])
-    submit = SubmitField('Зарегистрироваться')
+    surname         = StringField('Фамилия', validators=[DataRequired()])
+    name            = StringField('Имя', validators=[DataRequired()])
+    age             = IntegerField('Возраст', validators=[DataRequired()])
+    gender        = StringField('Пол', validators=[DataRequired()])
+    email           = EmailField('Почта', validators=[DataRequired()])
+    password        = PasswordField('Пароль', validators=[DataRequired()])
+    password_repeat = PasswordField('Повторите пароль',validators=[DataRequired()])
+    submit          = SubmitField('Зарегистрироваться')
